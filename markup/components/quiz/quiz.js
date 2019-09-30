5ce061c4f629ddb1018517a2d2bbea799b7441d1
Developer <denbudeyko@yandex.ru>
2019-09-30 08:51:24 +0300

    final quiz

--- markup/components/quiz/quiz.js
+++ markup/components/quiz/quiz.js
@@ -7,19 +7,31 @@ $(document).ready(function(){
 
   var quizCount = 1;
   var maxQuizCount = $('.quiz-item').length
+  var maxQuizToForm = maxQuizCount - 1;
 
 
   $('#quiz-next').on('click',function(){
-    if(quizCount >= maxQuizCount){
-      alert('vseo')
+    if(quizCount >= maxQuizToForm){
+
+      var lastClass = 'quiz-process-' + quizCount;
+      $('#quiz').removeClass()
+      quizCount = quizCount + 1;
+      var itemCount = quizCount - 1;
+      $('#quiz').addClass('quiz-process-' + quizCount)
+      $('.quiz-line__inner h6 span').removeClass('active')
+      $('.quiz-line__inner h6 span').eq(itemCount).addClass('active')
+      $('.quiz-item').removeClass('active')
+      $('.quiz-item').eq(itemCount).addClass('active')
+      $('.quiz').addClass('showform')
+      $('.quiz-title, .quiz-line, .quiz-steps, .quiz-controller').fadeOut()
+
+
     }
     else{
       var lastClass = 'quiz-process-' + quizCount;
       $('#quiz').removeClass()
       quizCount = quizCount + 1;
       var itemCount = quizCount - 1;
-  
-
       $('#quiz').addClass('quiz-process-' + quizCount)
       $('.quiz-line__inner h6 span').removeClass('active')
       $('.quiz-line__inner h6 span').eq(itemCount).addClass('active')
@@ -27,6 +39,8 @@ $(document).ready(function(){
       $('.quiz-item').eq(itemCount).addClass('active')
     
     }  
+
+
 
   })
 
