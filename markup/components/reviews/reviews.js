caf9fffe53a3d4d608bc03ad899a4cd5b725e796
Developer <denbudeyko@yandex.ru>
2019-09-30 09:09:08 +0300

    reviews

--- markup/components/reviews/reviews.js
+++ markup/components/reviews/reviews.js
@@ -8,4 +8,23 @@ $(document).ready(function(){
     dots: false
   })
 
+
+  $('.open-reviews').on('click',function(){
+
+    var image = $(this).parent().prev().children('img').attr('src')
+    var name = $(this).parent().children('h3.reviews-name').text();
+    var trans = $(this).parent().children('span.transp-name').text();;
+    var model = $(this).parent().children('span.model-name').text();
+    var fullText = $(this).prev().text()
+
+  
+    $('#rev-pop__img').attr('src',image);
+    $('#rev-pop__name').text(name)
+    $('#rev-pop__trans').text(trans);
+    $('#rev-pop__model').text(model)
+    $('#rev-pop__text').text(fullText)
+    
+
+  })
+
 })
