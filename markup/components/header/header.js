7072f2b43d1df4af318852da3be306b70798d1a1
Developer <denbudeyko@yandex.ru>
2019-10-01 08:05:12 +0300

    etc

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-  $('.offer-item__text').equalHeights()
-  $('.contract-item__title').equalHeights()
-  $('.offer-item__title').equalHeights()
-
+  if(window.innerWidth > 480){
+    $('.offer-item__text').equalHeights()
+    $('.contract-item__title').equalHeights()
+    $('.offer-item__title').equalHeights()
+  }
 })
